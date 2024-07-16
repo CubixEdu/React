f5640d5921435e4db95725b0720a16938921dfdd
kasznar <kasznardavid8@gmail.com>
2024-07-16 13:21:35 +0200

    create utils, shuffle deck

--- het2/src/App.js
+++ het2/src/App.js
@@ -1,42 +1,12 @@
 import './App.css';
 
-import CLUB from './assets/card-clubs-1.png';
-import DIAMOND from './assets/card-diamonds-1.png';
-import HEARTS from './assets/card-hearts-1.png';
-import SPADES from './assets/card-spades-1.png';
 import {Card} from "./components/Card";
 import {useEffect, useState} from "react";
+import {createShuffledDeck, update, getFlipped} from "./utils/deck";
 
-const cards = [CLUB, DIAMOND, HEARTS, SPADES].map((item, index) => ({
-    type: index,
-    img: item,
-}));
-const initialDeck = [...cards, ...cards].map((item, index) => ({
-    id: index,
-    img: item.img,
-    type: item.type,
-    isFlipped: false,
-    isFound: false,
-}));
-
-
-const update = (prevDeck, id, newValues) => {
-    return prevDeck.map((item) => {
-        if (item.id === id) {
-            return {
-                ...item,
-                ...newValues,
-            }
-        }
-
-        return item
-    })
-}
-
-const getFlipped = deck => deck.filter(item => item.isFlipped && !item.isFound);
 
 function App() {
-    const [deck, setDeck] = useState(initialDeck);
+    const [deck, setDeck] = useState(createShuffledDeck);
     const gameOver = !Boolean(deck.find(item => !item.isFound));
 
     useEffect(() => {
@@ -92,7 +62,7 @@ function App() {
     }
 
     const reset = () => {
-        setDeck(initialDeck);
+        setDeck(createShuffledDeck());
     }
 
 
