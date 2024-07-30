fa3c4e0ea10b2db9769942f4d4afc98fe578eee2
Dávid <david@dotis.io>
2024-07-30 14:07:12 +0200

    get random image url

--- het4/src/pages/AddNew.js
+++ het4/src/pages/AddNew.js
@@ -1,8 +1,18 @@
-import {Button, Stack, TextField} from "@mui/material";
-import {useEffect, useState} from "react";
+import {
+    Button,
+    FormControl,
+    IconButton,
+    InputAdornment,
+    InputLabel,
+    OutlinedInput,
+    Stack,
+    TextField
+} from "@mui/material";
+import {useState} from "react";
 import {useDogs} from "../contexts/dogContext";
 import {useNavigate} from "react-router-dom";
 import {Image} from "../utils/image";
+import ShuffleIcon from '@mui/icons-material/Shuffle';
 
 
 export const AddNew = () => {
@@ -11,9 +21,11 @@ export const AddNew = () => {
     const dogs = useDogs();
     const navigate = useNavigate();
 
-    useEffect(() => {
+
+    const handleGetRandomUrl = () => {
         Image.getRandomUrl().then(randomUrl => setUrl(randomUrl));
-    }, []);
+
+    }
 
     const handleAdd = () => {
         if (name && url) {
@@ -25,8 +37,27 @@ export const AddNew = () => {
 
     return (
         <Stack spacing={2}>
-            <TextField label="Name" value={name} onChange={(e) => setName(e.target.value)} />
-            <TextField label="Image Url" value={url} onChange={(e) => setUrl(e.target.value)} />
+            <TextField label="Name" value={name} onChange={(e) => setName(e.target.value)}/>
+            <FormControl>
+                <InputLabel htmlFor="image-url">Password</InputLabel>
+                <OutlinedInput
+                    id="image-url"
+                    label="Image Url"
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleGetRandomUrl}
+                                edge="end"
+                            >
+                                <ShuffleIcon/>
+                            </IconButton>
+                        </InputAdornment>
+                    }/>
+            </FormControl>
+
             <Button variant="contained" onClick={handleAdd}>Add</Button>
         </Stack>
     )
