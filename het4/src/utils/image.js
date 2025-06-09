3c7093ace17c91e3a8911429f8dc135a134fb5d7
dotisio <david@dotis.io>
2025-06-09 17:27:22 +0200

    het4

---
+++ het4/src/utils/image.js
@@ -0,0 +1,8 @@
+export const Image = {
+    getRandomUrl: async () => {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random")
+        const data = await response.json();
+
+        return data.message;
+    }
+};
